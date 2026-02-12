d23f06e1b96fc619e274f327b0876c6a0c18029f
David-Vaclavik <davidvaclavik.dv@gmail.com>
2026-02-12 19:23:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Message Board
+
+A simple message board application built with Node.js and Express, using EJS as the templating engine.
---
+++ app.js
@@ -0,0 +1,72 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "new", text: "New" },
+];
+
+// const users = ["Rose", "Cake", "Biff"];
+
+const messages = [
+  {
+    id: 1,
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    id: 2,
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// Define routes
+app.get("/", (req, res) => {
+  const formattedMessages = messages.map((msg) => ({
+    ...msg,
+    // toLocaleString() method has more options to format the date, maybe we can use it in the future
+    added: msg.added.toLocaleString("en-GB"),
+  }));
+  res.render("index", { links, messages: formattedMessages });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new", { links });
+});
+app.post("/new", (req, res) => {
+  const { author, message } = req.body;
+  messages.push({ id: messages.length + 1, text: message, user: author, added: new Date() });
+  res.redirect("/");
+});
+
+app.get("/message/:id", (req, res) => {
+  const { id } = req.params;
+  const message = messages.find((msg) => msg.id === parseInt(id));
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("messages", { links, message });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Listening on port ${PORT}!`);
+});
